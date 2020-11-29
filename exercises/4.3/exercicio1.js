e0bf57786008852b2d74a0def0ba37ba2101a7cc
Danrlley Rodrigues <danrlley.c.a.rodrigues@gmail.com>
2020-11-29 14:24:40 -0300

    Exercicio 1 & 2 Concluidos

---
+++ exercises/4.3/exercicio1.js
@@ -0,0 +1,10 @@
+let n = 10
+let t = ''
+
+for(let i2 = 0; i2 < n; i2 += 1){
+    t = ''
+    for(let i = 0; i < n; i += 1){
+        t += '*'
+    }
+    console.log(t)
+}
---
+++ exercises/4.3/exercicio2.js
@@ -0,0 +1,7 @@
+let n = 5
+let t = ''
+
+for(let i = 0; i < n; i += 1){
+    t += '*'
+    console.log(t)
+}
